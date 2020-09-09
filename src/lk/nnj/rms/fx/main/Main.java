abec73d1fb85089fe8aa7786bdb554b375749828
janithnishel <dhjanithnishel@gmail.com>
2020-09-09 07:20:31 +0530

    75% finished project

--- src/lk/nnj/rms/fx/main/Main.java
+++ src/lk/nnj/rms/fx/main/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/lk/nnj/rms/fx/view/PlaceOrderForm.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/lk/nnj/rms/fx/view/ManageDelivery.fxml"));
         primaryStage.setTitle("Restaurant Management System");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1328, 693));
         primaryStage.show();
     }
 
